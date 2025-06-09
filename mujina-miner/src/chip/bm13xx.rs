56b15d9fb83a0a34470b0f8bd86f39cea8478fda
Ryan Kuester <rkuester@insymbols.com>
2025-06-08 19:20:33 -0500

    wip: flush out chip communication

--- mujina-miner/src/chip/bm13xx.rs
+++ mujina-miner/src/chip/bm13xx.rs
@@ -19,7 +19,7 @@ pub enum RegisterAddress {
     // FastUartConfiguration = 0x28,
     // Pll1Parameter = 0x60,
     // VersionRolling = 0xa4,
-    // RegA8 = 0xa8,
+    RegA8 = 0xa8,
 }
 
 pub enum Register {
@@ -27,6 +27,9 @@ pub enum Register {
         chip_id: u16,
         core_count: u8,
         address: u8,
+    },
+    RegA8 {
+        unknown: u32,
     },
 }
 
@@ -38,8 +41,25 @@ impl Register {
                 core_count: u8::from_be(bytes[2]),
                 address: u8::from_be(bytes[3]),
             },
+            RegisterAddress::RegA8 => Register::RegA8 { 
+                unknown: u32::from_be_bytes(*bytes)
+            },
         }
     }
+}
+
+#[repr(u8)]
+enum CommandFlagsType {
+    // Job = 1,
+    Command = 2,
+}
+
+#[repr(u8)]
+enum CommandFlagsCmd {
+    // SetAddress = 0,
+    WriteRegisterOrJob = 1,
+    ReadRegister = 2,
+    // ChainInactive = 3,
 }
 
 pub enum Command {
@@ -48,75 +68,43 @@ pub enum Command {
         chip_address: u8,
         register_address: RegisterAddress,
     },
+    WriteRegister {
+        all: bool,
+        chip_address: u8,
+        register: Register,
+    },
 }
 
-struct CommandFieldBuilder {
-    field: u8,
-}
 
-#[repr(u8)]
-enum CommandFieldType {
-    // Job = 1,
-    Command = 2,
-}
-
-#[repr(u8)]
-enum CommandFieldCmd {
-    // SetAddress = 0,
-    // WriteRegisterOrJob = 1,
-    ReadRegister = 2,
-    // ChainInactive = 3,
-}
-
-impl CommandFieldBuilder {
-    fn new() -> Self {
-        Self { field: 0 }
+impl Command {
+    fn build_flags(typ: CommandFlagsType, all: bool, cmd: CommandFlagsCmd) -> u8 {
+        let mut flags = 0u8;
+        let field = flags.view_bits_mut::<Lsb0>();
+        field[5..7].store(typ as u8);
+        field[4..5].store(all as u8);
+        field[0..4].store(cmd as u8);
+        flags
     }
 
-    fn with_type(mut self, command_type: CommandFieldType) -> Self {
-        let view = self.field.view_bits_mut::<Lsb0>();
-        view[5..7].store(command_type as u8);
-        self
-    }
+    fn encode(&self, dst: &mut BytesMut) {
+        match self {
+            Command::ReadRegister { all, .. } => {
+                dst.put_u8(Self::build_flags(
+                    CommandFlagsType::Command,
+                    *all,
+                    CommandFlagsCmd::ReadRegister,
+                ));
 
-    fn with_type_for_command(self, command: &Command) -> Self {
-        self.with_type(match command {
-            Command::ReadRegister { .. } => CommandFieldType::Command,
-        })
-    }
-
-    fn with_all(mut self, all: &bool) -> Self {
-        let view = self.field.view_bits_mut::<Lsb0>();
-        view[4..5].store(*all as u8);
-        self
-    }
-
-    fn with_all_for_command(self, command: &Command) -> Self {
-        self.with_all(match command {
-            Command::ReadRegister { all, .. } => all,
-        })
-    }
-
-    fn with_cmd(mut self, cmd: CommandFieldCmd) -> Self {
-        let view = self.field.view_bits_mut::<Lsb0>();
-        view[0..4].store(cmd as u8);
-        self
-    }
-
-    fn with_cmd_for_command(self, command: &Command) -> Self {
-        self.with_cmd(match command {
-            Command::ReadRegister { .. } => CommandFieldCmd::ReadRegister,
-        })
-    }
-
-    fn for_command(self, command: &Command) -> Self {
-        self.with_type_for_command(command)
-            .with_all_for_command(command)
-            .with_cmd_for_command(command)
-    }
-
-    fn build(self) -> u8 {
-        self.field
+                TODO
+            }
+            Command::WriteRegister { all, .. } => {
+                dst.put_u8(Self::build_flags(
+                    CommandFlagsType::Command,
+                    *all,
+                    CommandFlagsCmd::WriteRegisterOrJob,
+                ));
+            }
+        }
     }
 }
 
@@ -181,21 +169,7 @@ impl Encoder<Command> for FrameCodec {
         const COMMAND_PREAMBLE: &[u8] = &[0x55, 0xaa];
         dst.put_slice(COMMAND_PREAMBLE);
 
-        let command_field = CommandFieldBuilder::new().for_command(&command).build();
-        dst.put_u8(command_field);
-
-        match command {
-            Command::ReadRegister {
-                all: _,
-                chip_address: address,
-                register_address: register,
-            } => {
-                const LENGTH: u8 = 5;
-                dst.put_u8(LENGTH);
-                dst.put_u8(address);
-                dst.put_u8(register as u8);
-            }
-        }
+        command.encode(dst);
 
         let crc = crc5(&dst[2..]);
         dst.put_u8(crc);
